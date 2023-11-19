1ecb66d2904a87f5fd2007c50dcd35ee52e4335f
cichowlasp <cichowlas.p@gmail.com>
2023-11-19 22:29:51 +0100

    added printing package data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,39 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use pcap::{Capture, Packet};
+use std::fs;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_pcap_file(file_path: &str) -> Result<(), String> {
+    let mut cap = Capture::from_file(file_path).unwrap();
+
+    while let Ok(packet) = cap.next_packet() {
+        println!("Packet captured with length {}", packet.header.len);
+
+        // Print the packet data
+        print_packet_data(packet);
+    }
+
+    Ok(())
 }
+
+fn print_packet_data(packet: Packet) {
+    // Assuming Ethernet frame for simplicity
+    let eth_bytes = packet.data;
+
+    // Print the hexadecimal representation of packet data
+    for (i, byte) in eth_bytes.iter().enumerate() {
+        print!("{:02X} ", byte);
+        if (i + 1) % 16 == 0 {
+            println!();
+        }
+    }
+    println!();
+}
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_pcap_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
